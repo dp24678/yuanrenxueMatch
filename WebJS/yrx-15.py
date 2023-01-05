c5ac52ca0f45f33b402ba8e669af5d9fd21f608f
Dapan <wali666@qq.com>
2023-01-05 11:09:30 +0800

    已完成

--- WebJS/yrx-15.py
+++ WebJS/yrx-15.py
@@ -3,6 +3,11 @@ import time
 import pywasm
 import requests
 
+""" 
+关键点：使用 WebAssembly 技术将关键参数生成方法加密
+https://demo.dandelioncloud.cn/article/details/1604647408942399490
+https://zhuanlan.zhihu.com/p/434831594
+"""
 
 def m():
     """
@@ -17,35 +22,10 @@ def m():
     r = vm.exec('encode', [t1, t2])
     return f"{r}|{t1}|{t2}"
 
-import requests
 
 cookies = {
-    # 'tk': '-1309588185006210753',
     'sessionid': '50pq3l1p9e4fgxtqxzkst5a8jkike0hv',
 }
-
-headers = {
-    'authority': 'match.yuanrenxue.com',
-    'accept': '*/*',
-    'accept-language': 'zh-CN,zh;q=0.9',
-    'cache-control': 'no-cache',
-    # 'cookie': 'Hm_lvt_c99546cf032aaa5a679230de9a95c7db=1672884000; tk=-1309588185006210753; sessionid=50pq3l1p9e4fgxtqxzkst5a8jkike0hv; Hm_lvt_9bcbda9cbf86757998a2339a0437208e=1672887649; Hm_lpvt_9bcbda9cbf86757998a2339a0437208e=1672887649; Hm_lpvt_c99546cf032aaa5a679230de9a95c7db=1672887649',
-    'pragma': 'no-cache',
-    'referer': 'https://match.yuanrenxue.com/match/15',
-    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-}
-
-response = requests.get('https://match.yuanrenxue.com/static/match/match15/main.wasm', cookies=cookies, headers=headers)
-with open("a.wasm", "wb") as f:
-    f.write(response.content)
-
-
 headers = {
     'authority': 'match.yuanrenxue.com',
     'accept': 'application/json, text/javascript, */*; q=0.01',
@@ -63,8 +43,11 @@ headers = {
     'user-agent': 'yuanrenxue.project',
     'x-requested-with': 'XMLHttpRequest',
 }
-
 s = requests.Session()
+response = s.get('https://match.yuanrenxue.com/static/match/match15/main.wasm', cookies=cookies, headers=headers)
+with open("a.wasm", "wb") as f:
+    f.write(response.content)
+
 prices = []
 for i in range(1, 6):
     params = {
